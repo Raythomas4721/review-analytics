b050a10b836c71c3c4e4a44b25b421f06b3c5fe9
Ray Cheng <ray123321123321@gmail.com>
2022-02-15 05:17:17 -0800

    calc avg of review

--- read.py
+++ read.py
@@ -8,5 +8,10 @@ with open('reviews.txt','r') as f:
 			print(len(data))	
 print(len(data))
 
-print(data[0])
-print('--------------------------')
+print('檔案讀取完畢,總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+		sum_len = sum_len + len(d)
+
+print('留言平均長度為', sum_len/len(data))
